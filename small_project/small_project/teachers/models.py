336993a0819b8b95f497f51a370ed43c4dd560c1
Hoang Nguyen <74847239+Bibo1604@users.noreply.github.com>
2022-07-28 11:34:20 +0700

    delete

--- small_project/small_project/teachers/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Teacher(models.Model):
-    name = models.CharField(max_length=100)
-    birthday = models.DateField()
-
-    def __str__(self):
-        return self.name
